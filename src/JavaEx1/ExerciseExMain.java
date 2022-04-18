c5bbc958356c059352eb9f4ee639ca0bd13202ed
liverbird <ehdgns07@gmail.com>
2022-04-18 18:28:30 +0900

    firstday finish

---
+++ src/JavaEx1/ExerciseExMain.java
@@ -0,0 +1,9 @@
+package JavaEx1;
+
+public class ExerciseExMain {
+    public static void main(String[] args) {
+        Three th = new Three();
+        th.Three();
+        th.four();
+    }
+}
---
+++ src/JavaEx1/Three.java
@@ -0,0 +1,20 @@
+package JavaEx1;
+
+public class Three {
+    public void Two(){
+        int numOfApples = 123;
+        int sizeOfBucket = 10; // 바구니의 크기(바구니에 담을 수 있는 사과의 개수)
+        int numOfBucket = (13); // 모든 사과를 담는데 필요한 바구니의 수
+        System.out.println("필요한 바구니의 수 :"+numOfBucket);
+    }
+
+    public void Three(){
+        int num = 10;
+        System.out.println((num==0)? "0":(num<0)? "음수":"양수");
+    }
+
+    public void four(){
+        int num = 456;
+        System.out.println(num/100*100);
+    }
+}
