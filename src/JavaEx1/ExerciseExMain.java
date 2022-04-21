aa8c45421f0afd34b7e4798b309c621044dddb2f
liverbird <ehdgns07@gmail.com>
2022-04-21 13:48:58 +0900

    third day finish

--- src/JavaEx1/ExerciseExMain.java
+++ src/JavaEx1/ExerciseExMain.java
@@ -3,14 +3,18 @@ package JavaEx1;
 public class ExerciseExMain {
     public static void main(String[] args) {
         Three th = new Three();
+        Four four = new Four();
 //        th.Three();
 //        th.four();
 //        th.five();
 //        th.six();
 //        th.seven();
-        th.eight();
-        th.nine();
-        th.ten();
+//        th.eight();
+//        th.nine();
+//        th.ten();
+        four.one();
+        four.two();
+        four.three();
     }
 
 }
--- src/JavaEx1/Four.java
+++ src/JavaEx1/Four.java
@@ -1,19 +1,73 @@
 package JavaEx1;
 
 public class Four {
-//    [4-1] 다음의 문장들을 조건식으로 표현하라.
-//        1. int형 변수 x가 10보다 크고 20보다 작을 때 true인 조건식
-//2. char형 변수 ch가 공백이나 탭이 아닐 때 true인 조건식
-//3. char형 변수 ch가 ‘x' 또는 ’X'일 때 true인 조건식
-//4. char형 변수 ch가 숫자(‘0’~‘9’)일 때 true인 조건식
-//5. char형 변수 ch가 영문자(대문자 또는 소문자)일 때 true인 조건식
-//6. int형 변수 year가 400으로 나눠떨어지거나 또는 4로 나눠떨어지고 100으로 나눠떨어지지
-//    않을 때 true인 조건식
-//7. boolean형 변수 powerOn가 false일 때 true인 조건식
-//8. 문자열 참조변수 str이 “yes”일 때 true인 조건식
 
+    public void one() {
+        int x = 15;
+        int year = 100;
+        boolean result;
+        if (x > 10 && x < 20) {
+            result = true;
+            System.out.println(result+"1");
+        }
+        char ch = 0x10;
 
-//[4-2] 1부터 20까지의 정수 중에서 2 또는 3의 배수가 아닌 수의 총합을 구하시오.
+        if (ch != 0x20  && ch != 0x09) {
+            result = true;
+            System.out.println(result+"2");
+        }
+        ch = 'x';
+        if (ch == 0x58 || ch == 0x78) {
+            result = true;
+            System.out.println(result+"3");
+        }
+        ch = '7';
+        if (ch > 0x2f && ch < 0x3a) {
+            result = true;
+            System.out.println(result+"4");
+        }
+        ch = 'a';
+        if ((ch > 0x40 && ch < 0x5b) || (ch > 0x60 && ch < 0x7b)) {
+            result = true;
+            System.out.println(result+"5");
+        }
+        year = 8;
+        if (((year % 400 == 0) || (year % 4 == 0)) && (year % 100 != 0)) {
+            result = true;
+            System.out.println(result+"6");
+        }
+        boolean powerOn = false;
+        if (!powerOn) {
+            result = true;
+            System.out.println(result+"7");
+        }
+        String str = "yes";
+        if (str.equals("yes")) {
+            result = true;
+            System.out.println(result+"8");
+        }
+    }
 
-//[4-3] 1+(1+2)+(1+2+3)+(1+2+3+4)+...+(1+2+3+...+10)의 결과를 계산하시오.
+    public void two() {
+        int result = 0;
+
+        for (int i = 1; i < 21; i++) {
+            if ((i % 2 != 0) && (i % 3 != 0)) {
+                result += i;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    public void three() {
+        int result1 = 0;
+        int result2 = 0;
+        for (int i = 1; i < 11; i++) {
+            result1 += i;
+            result2 += result1;
+        }
+        System.out.println(result2);
+    }
+
 }
